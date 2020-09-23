cf626c3bdc51923c22bddb17d3f3a4ac3cab7b2b
Oleksii Levchenko <>
2020-09-23 19:38:13 +0300

    Setup project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ImageFinder } from '../ImageFinder';
+
+export const App: React.FC = () => {
+    return <ImageFinder />;
+};
---
+++ src/components/ImageFinder/ImageFinder.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { SImageFinder } from './ImageFinder.sc';
+
+export const ImageFinder: React.FC = () => {
+    return <SImageFinder>Image Finder</SImageFinder>;
+};
